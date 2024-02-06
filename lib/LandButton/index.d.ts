b3f3c7407aad6316778e135fd116c7b67d893dd1
minnasu <minnasu@tencent.com>
2024-02-06 15:11:31 +0800

    配置

--- lib/LandButton/index.d.ts
+++ lib/LandButton/index.d.ts
@@ -1,4 +1,12 @@
-import React from 'react';
-export type LandButtonProps = {};
+import React, { CSSProperties } from 'react';
+export type LandButtonProps = {
+    text?: string;
+    icon?: React.ReactNode;
+    type?: 'default' | 'primary' | 'disabled' | 'default-stroke' | 'primary-stroke' | 'disabled-stroke';
+    width?: number | string;
+    onClick?: () => void;
+    style?: CSSProperties;
+    className?: string;
+};
 declare const LandButton: React.FC<LandButtonProps>;
 export default LandButton;
