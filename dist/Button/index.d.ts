ffed5f073ca9988ab9f45e220f849c39c5e6b095
minnasu <minnasu@tencent.com>
2024-02-22 12:54:28 +0800

    update

---
+++ dist/Button/index.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="react" />
+import './style';
+import { ButtonProps } from './interface';
+declare const ButtonComponent: import("react").ForwardRefExoticComponent<ButtonProps & import("react").RefAttributes<unknown>>;
+export default ButtonComponent;
+export { ButtonProps };
