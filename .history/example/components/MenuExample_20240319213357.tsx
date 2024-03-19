ebef6ca0aee98ccc432f1875685ce4e1a294128e
minnasu <minnasu@tencent.com>
2024-03-19 21:37:50 +0800

    refactor: Menu扩展

---
+++ .history/example/components/MenuExample_20240319213357.tsx
@@ -0,0 +1,177 @@
+import React from "react";
+import Title from "../../packages/Title";
+import Menu, { ClickType } from "../../packages/Menu";
+
+export default function MenuExample() {
+  return (
+    <div className="flex column gap-24">
+      <div className="flex column gap-12">
+        <Title title="基本用法" type="h3" />
+        <div className="flex justify-center gap-24 border p-32">
+          <Menu
+            data={[
+              { key: 1, title: "Navigation 1", clickType: ClickType.SELF },
+              { key: 2, title: "Navigation 2", clickType: ClickType.SELF },
+              { key: 3, title: "Navigation 3", clickType: ClickType.SELF },
+              { key: 4, title: "Navigation 4", clickType: ClickType.SELF },
+              { key: 5, title: "Navigation 5", clickType: ClickType.SELF },
+            ]}
+            actived={1}
+          />
+        </div>
+        <Title title="角标" type="h3" />
+        <div className="flex justify-center gap-24 border p-32">
+          <Menu
+            data={[
+              { key: 1, title: "Navigation 1", clickType: ClickType.SELF },
+              {
+                key: 2,
+                title: "Navigation 2",
+                clickType: ClickType.SELF,
+                isNew: true,
+              },
+              {
+                key: 3,
+                title: "Navigation 3",
+                clickType: ClickType.SELF,
+                isNew: "Hot",
+              },
+              { key: 4, title: "Navigation 4", clickType: ClickType.SELF },
+              { key: 5, title: "Navigation 5", clickType: ClickType.SELF },
+            ]}
+            actived={1}
+          />
+        </div>
+        <Title title="不可点击 & 禁用" type="h3" />
+        <div className="flex justify-center gap-24 border p-32">
+          <Menu
+            data={[
+              {
+                key: 1,
+                title: "Navigation(cursor)",
+                clickType: ClickType.SELF,
+              },
+              {
+                key: 2,
+                title: "Navigation(default)",
+                clickType: ClickType.SIMPLE,
+              },
+              {
+                key: 2,
+                title: "Navigation(not-allowed)",
+                clickType: ClickType.DISABLED,
+              },
+            ]}
+            actived={1}
+          />
+        </div>
+        <Title title="包含下拉框" type="h3" />
+        <div className="flex justify-center gap-24 border p-32">
+          <Menu
+            data={[
+              {
+                key: 1,
+                title: "Navigation(cursor)",
+                clickType: ClickType.SELF,
+              },
+              {
+                key: 2,
+                title: "Navigation(default)",
+                clickType: ClickType.SELF,
+              },
+            ]}
+            actived={1}
+          />
+        </div>
+        <Title title="列向导航" type="h3" />
+        <div className="flex justify-center gap-24 border p-32">
+          <Menu
+            data={[
+              { key: 1, title: "Navigation 1", clickType: ClickType.SELF },
+              { key: 2, title: "Navigation 2", clickType: ClickType.SELF },
+              { key: 3, title: "Navigation 3", clickType: ClickType.SELF },
+              { key: 4, title: "Navigation 4", clickType: ClickType.SELF },
+              { key: 5, title: "Navigation 5", clickType: ClickType.SELF },
+            ]}
+            actived={3}
+            direction="column"
+          />
+        </div>
+        <Title title="导航选中风格" type="h3" />
+        <div className="flex column items-center gap-24 border p-32">
+          <Menu
+            data={[
+              { key: 1, title: "Navigation 1", clickType: ClickType.SELF },
+              { key: 2, title: "Navigation 2", clickType: ClickType.SELF },
+              { key: 3, title: "Navigation 3", clickType: ClickType.SELF },
+            ]}
+            actived={1}
+          />
+          <Menu
+            data={[
+              { key: 1, title: "Navigation 1", clickType: ClickType.SELF },
+              { key: 2, title: "Navigation 2", clickType: ClickType.SELF },
+              { key: 3, title: "Navigation 3", clickType: ClickType.SELF },
+            ]}
+            actived={2}
+            theme="background"
+          />
+          <Menu
+            data={[
+              { key: 1, title: "Navigation 1", clickType: ClickType.SELF },
+              { key: 2, title: "Navigation 2", clickType: ClickType.SELF },
+              { key: 3, title: "Navigation 3", clickType: ClickType.SELF },
+            ]}
+            actived={3}
+            theme="line"
+          />
+        </div>
+        <div className="flex justify-center gap-24 border p-32">
+          <Menu
+            data={[
+              { key: 1, title: "Navigation 1", clickType: ClickType.SELF },
+              { key: 2, title: "Navigation 2", clickType: ClickType.SELF },
+              { key: 3, title: "Navigation 3", clickType: ClickType.SELF },
+            ]}
+            actived={1}
+            direction="column"
+          />
+          <Menu
+            data={[
+              { key: 1, title: "Navigation 1", clickType: ClickType.SELF },
+              { key: 2, title: "Navigation 2", clickType: ClickType.SELF },
+              { key: 3, title: "Navigation 3", clickType: ClickType.SELF },
+            ]}
+            actived={2}
+            theme="background"
+            direction="column"
+          />
+          <Menu
+            data={[
+              { key: 1, title: "Navigation 1", clickType: ClickType.SELF },
+              { key: 2, title: "Navigation 2", clickType: ClickType.SELF },
+              { key: 3, title: "Navigation 3", clickType: ClickType.SELF },
+            ]}
+            actived={3}
+            theme="line"
+            direction="column"
+          />
+        </div>
+        <Title title="带导航" type="h3" />
+        <div className="flex justify-center gap-24 border p-32">
+          <Menu
+            data={[
+              {
+                key: 1,
+                title: "Title1",
+                subTitle: "subTitle",
+                clickType: ClickType.SELF,
+              },
+            ]}
+            actived={3}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ .history/packages/Menu_20240319205722.tsx
@@ -0,0 +1,183 @@
+import React, { CSSProperties, useState } from "react";
+import styled from "styled-components";
+
+export enum ClickType {
+  /* 当前页面切换 */
+  SELF = "self",
+  /* 外部跳转 */
+  OTHERS = "others",
+  /* 不可点击项（仅作为下拉框导航项的整合，通过下拉框子项切换，鼠标悬浮样式为 default） */
+  SIMPLE = 'simple',
+  /* 禁用 */
+  DISABLED = 'disabled',
+}
+export type MenuItemType = {
+  key: number;
+  title?: string;
+  icon?: string | React.ReactNode;
+  isNew?: string | boolean;
+  href?: string;
+  clickType: ClickType;
+  dropData?: MenuItemType[];
+};
+
+export type MenuProps = {
+  actived?: number;
+  data?: MenuItemType[];
+  direction?: 'row' | 'column';
+  itemStyle?: CSSProperties;
+  itemClassName?: string;
+  style?: CSSProperties;
+  className?: string;
+};
+
+const Menu: React.FC<MenuProps> = ({
+  actived,
+  data,
+  direction = "row",
+  itemStyle,
+  itemClassName,
+  style,
+  className,
+}) => {
+  const [newActived, setNewActived] = useState<number>(actived);
+  const handleChangeTab = (item: MenuItemType) => {
+    item.clickType === ClickType.SELF && setNewActived(item.key);
+  };
+  return (
+    <StyledMenu
+      className={`land-menu ${className}`}
+      style={style}
+      direction={direction}
+    >
+      {data?.map((item) => (
+        <div className="land-nav-item">
+          <a
+            role="button"
+            key={item.key}
+            className={`land-nav-link ${
+              item.clickType === ClickType.SIMPLE ? "simple" : ""
+            } ${item.clickType === ClickType.DISABLED ? "disabled" : ""} ${
+              newActived === item.key ? "actived" : ""
+            } ${itemClassName}`}
+            style={itemStyle}
+            data-title={item.title}
+            onClick={() => handleChangeTab?.(item)}
+          >
+            {typeof item.icon === "string" ? (
+              <img src={item.icon} className="land-nav-icon" />
+            ) : (
+              item.icon
+            )}
+            {item.title}
+            {item.isNew && (
+              <i className="land-nav-new">
+                {typeof item.isNew === "boolean" ? "NEW" : item.isNew}
+              </i>
+            )}
+          </a>
+        </div>
+      ))}
+    </StyledMenu>
+  );
+};
+
+const StyledMenu = styled.div<{
+  direction?: string;
+}>`
+  display: flex;
+  flex-direction: ${(props) => props.direction};
+  gap: var(--gap-4);
+  height: ${(props) => (props.direction === "row" ? "100%" : "")};
+  .land-nav-item {
+    position: relative;
+    height: ${(props) => (props.direction === "row" ? "100%" : "")};
+    width: ${(props) => (props.direction === "column" ? "100%" : "")};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .land-nav-link {
+    padding: var(--padding-medium);
+    border-radius: var(--radius-4);
+    color: transparent;
+    font-weight: 600;
+    font-weight: 400;
+    font-size: var(--font-content-large);
+    white-space: nowrap;
+    transition: background-color var(--transition-15) linear;
+    cursor: pointer;
+    &:hover {
+      background-color: var(--color-bg-1);
+      &::before {
+        color: var(--color-text-1);
+        font-weight: 600;
+      }
+    }
+    &::before {
+      content: attr(data-title);
+      position: absolute;
+      left: 0;
+      top: 0;
+      height: 100%;
+      width: 100%;
+      color: var(--color-text-3);
+      text-align: center;
+      line-height: 40px;
+    }
+    &::after {
+      content: "";
+      display: block;
+      position: absolute;
+      right: ${(props) => (props.direction === "row" ? "50%" : "-4px")};
+      bottom: ${(props) => (props.direction === "row" ? "0" : "50%")};
+      width: 12px;
+      height: 2px;
+      transform: ${(props) =>
+        props.direction === "row"
+          ? `translateX(50%)`
+          : "translateY(50%) rotate(90deg)"};
+      transform-origin: center center;
+      border-radius: 1px;
+      background-color: var(--color-primary-1);
+      transition: opacity var(--transition-15) linear;
+      opacity: 0;
+    }
+    &.actived {
+      &::before {
+        color: var(--color-text-1);
+        font-weight: 600;
+      }
+      &::after {
+        opacity: 1;
+      }
+    }
+    &.simple {
+      cursor: default;
+    }
+    &.disabled {
+      pointer-events: none;
+      opacity: var(--opacity-68);
+    }
+
+    .land-nav-icon {
+      width: 14px;
+      height: 14px;
+    }
+    .land-nav-new {
+      position: absolute;
+      padding: var(--padding-small);
+      top: -12px;
+      right: -8px;
+      font-size: var(--font-content-small);
+      background-color: var(--color-link-1);
+      color: var(--color-link-6);
+      font-weight: 600;
+      border-radius: 3px;
+      box-sizing: border-box;
+      transform: scale(0.65);
+    }
+  }
+`;
+
+export default Menu;
