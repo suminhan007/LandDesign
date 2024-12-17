685a9470386e4aa3af8be0446f037b837a2d0427
minnasu <minnasu@tencent.com>
2024-12-17 19:40:08 +0800

    perf: 优化依赖

--- example/App.tsx
+++ example/App.tsx
@@ -9,10 +9,11 @@ import { ClickType } from "../packages/Menu";
 import Components from "./Components";
 import Design from "./Design";
 import Animations from "./Animations";
+import Switch from "../packages/Switch";
 
 function App() {
   const [page, setPage] = useState<number | string>(3);
-  // const [dark, setDark] = useState<boolean>(false);
+  const [dark, setDark] = useState<boolean>(false);
   return (
     <div className="flex column" style={{ height: "100vh" }}>
       <Header
@@ -20,20 +21,26 @@ function App() {
         name={<Title title="Component Demo Lib" />}
         menuProps={{
           data: [
-            { key: 1, title: '使用指南', clickType: ClickType.SELF },
-            { key: 2, title: '设计语言', clickType: ClickType.SELF },
+            { key: 1, title: "使用指南", clickType: ClickType.SELF },
+            { key: 2, title: "设计语言", clickType: ClickType.SELF },
             {
               key: 3,
               title: "组件",
               clickType: ClickType.SELF,
             },
-            { key: 4, title: '动画', clickType: ClickType.SELF },
+            { key: 4, title: "动画", clickType: ClickType.SELF },
           ],
           active: page,
           onChange: (item) => setPage(item.key),
         }}
         align="end"
-      // rightComponent={<Switch label="使用黑色主题" checkedLabel="使用黑色主题" onChange={checked => setDark(checked)} />}
+        rightComponent={
+          <Switch
+            label="使用黑色主题"
+            checkedLabel="使用黑色主题"
+            onChange={(checked) => setDark(checked)}
+          />
+        }
       />
       <div className="flex-1 flex" style={{ height: "0" }}>
         {page === 2 && <Design />}
